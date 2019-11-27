9ed7778b4786aab4f82d94a9da95b91e531c160c
Артем <AKarpetc@kazzinc.com>
2019-11-27 14:48:32 +0600

    Исправл замечания осталось убрать дублирующие и ненужные элементы

---
+++ source/js/app.js
@@ -0,0 +1,45 @@
+const desktopWidth = "960";
+var isOpenMenu = false;
+document.addEventListener("DOMContentLoaded", function () {
+
+  let buttonClose = document.querySelector(".main-nav__toogle");
+  let nav = document.querySelector(".main-nav__list");
+  let header = document.querySelector(".page-header__logo");
+
+  function menegeMenu() {
+
+
+    if (isOpenMenu) {
+
+      buttonClose.classList.remove("main-nav__toogle--burger");
+      nav.classList.remove("visually-hidden");
+      header.classList.remove("page-header__logo--close");
+
+    } else {
+
+      buttonClose.classList.add("main-nav__toogle--burger");
+      nav.classList.add("visually-hidden");
+      header.classList.add("page-header__logo--close");
+
+    }
+
+    if (window.innerWidth > desktopWidth && !isOpenMenu) {
+      nav.classList.remove("visually-hidden");
+    }
+
+  }
+
+
+  buttonClose.addEventListener("click", function (e) {
+    isOpenMenu = !isOpenMenu;
+    menegeMenu();
+
+  });
+
+  window.onresize = function () {
+    menegeMenu();
+  };
+
+  menegeMenu();
+
+});
