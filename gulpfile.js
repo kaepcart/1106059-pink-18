a3b613b6372f4aad9593d50f98ec56f9f2d712ef
Артем <AKarpetc@kazzinc.com>
2019-12-27 12:32:03 +0600

    Добавил в конфигурационный файл gulp webp и start

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
-var rename=require("gulp-rename");
+var rename = require("gulp-rename");
 
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -39,6 +42,32 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
   gulp.watch("source/js/*.js").on("change", server.reload);
 
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin[
+
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+
+      imagemin.jpegtran({
+        progressive: true
+      }),
+
+      imagemin.svgo()
+    ])
+    .pipe(gulp.dest("source/img"))
+
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
